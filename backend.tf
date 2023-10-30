f842ee9db9795f326ba32cfb6c78eb6de371469a
diorng78 <diorng78@yahoo.com.sg>
2023-10-30 22:32:39 +0800

    New Commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "dior-s3"
-    key    = "path/to/my/key"
+    bucket = "sctp-ce3-tfstate-bucket-1"
+    key    = "dior.tfstate"
     region = "us-east-1"
   }
 }
--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "queue" {
     }
 
     actions   = ["sqs:SendMessage"]
-    resources = ["arn:aws:sqs:*:*:s3-event-notification-queue"]
+    resources = ["arn:aws:sqs:*:*:dior-event-notification-queue"]
 
     condition {
       test     = "ArnEquals"
@@ -19,12 +19,12 @@ data "aws_iam_policy_document" "queue" {
 }
 
 resource "aws_sqs_queue" "queue" {
-  name   = "s3-event-notification-queue"
+  name   = "dior-event-notification-queue"
   policy = data.aws_iam_policy_document.queue.json
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "your-bucket-name"
+  bucket = "sctp-dior-s3"
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
@@ -33,6 +33,6 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   queue {
     queue_arn     = aws_sqs_queue.queue.arn
     events        = ["s3:ObjectCreated:*"]
-    filter_suffix = ".log"
+
   }
 }
